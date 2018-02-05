aabe6dcafde605f0797f35f7b659d9d1f936bc58
Prem Patel <prempatel40p@gmail.com>
2018-02-05 07:24:12 +0530

    list view and custom adapter.

--- app/src/main/java/com/premp/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/premp/coderswag/controller/MainActivity.kt
@@ -3,11 +3,19 @@ package com.premp.coderswag.controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.premp.coderswag.R
+import com.premp.coderswag.adapters.CategoryAdapter
+import com.premp.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/premp/coderswag/modle/Category.kt
+++ app/src/main/java/com/premp/coderswag/modle/Category.kt
@@ -3,4 +3,8 @@ package com.premp.coderswag.modle
 /**
  * Created by premp on 05-02-2018.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
